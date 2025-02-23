23687ff2a479a4cc74cba483ec27f6e3553bd812
Bertie Wilson <19255166+bertiewils@users.noreply.github.com>
2025-02-23 23:13:09 +0000

    fix: sass install

--- src/dart-sass/NOTES.md
+++ src/dart-sass/NOTES.md
@@ -3,3 +3,5 @@
 ## OS Support
 
 This Feature should work on recent versions of Debian/Ubuntu-based distributions with `curl` installed.
+
+`bash` is required to execute the `install.sh` script.
--- src/dart-sass/install.sh
+++ src/dart-sass/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Determine architecture
@@ -8,19 +8,21 @@ case "$(uname -m)" in
     *)             ARCH=x64 ;;
 esac
 
+VERSION=${VERSION:-"latest"}
+
 # Determine latest version
-if ${VERSION} == "latest"; then
+if [[ ${VERSION} == "latest" ]]; then
     VERSION=$(curl -s https://api.github.com/repos/sass/dart-sass/releases/latest | grep tag_name | cut -d '"' -f 4)
 fi
 
 echo "Installing the ${ARCH} ${VERSION} version of dart-sass"
 
 
-FILENAME=dart-sass-${DART_SASS_VERSION}-linux-${ARCH}.tar.gz
+FILENAME=dart-sass-${VERSION}-linux-${ARCH}.tar.gz
 
-curl -LJO https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/${FILENAME}
+curl -LJO "https://github.com/sass/dart-sass/releases/download/${VERSION}/${FILENAME}"
 
-tar -xf ${FILENAME}
+tar -xf "${FILENAME}"
 
 cp -r dart-sass/* /usr/local/bin
 
